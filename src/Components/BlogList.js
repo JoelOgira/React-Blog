1d840cd791636132a25b1a39c3b5d3fc0e9e86f7
JoelOgira-2020 <joelogira@gmail.com>
2022-06-12 21:59:28 +0300

    Added useEffect hook

--- src/Components/BlogList.js
+++ src/Components/BlogList.js
@@ -7,9 +7,9 @@ const BlogList = ({blogs, title, onClickDelete}) => {
                     <div className="card mb-2">
                         <div className="card-body">
                             <h3 className="card-title py-2">{blog.title}</h3>
+                            <i onClick={() => onClickDelete(blog.id)} className="fas fa-trash-can pt-2" style={{ color: 'green', float: 'right', cursor: 'pointer' }}></i>
                             <p className="py-1 card-text"><span className="text-success">Written By: </span> {blog.author}</p>
                         </div>
-                        <i onClick={() => onClickDelete(blog.id)} className="fas fa-trash-can pt-2" style={{ color: 'green', float: 'right', cursor: 'pointer', right: '5px' }}></i>
                     </div>
                 </div>
             ))}
